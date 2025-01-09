4da3740b18a02eede848d90b91337d96a30d0f44
ashleywashere <ashleytrades412@gmail.com>
2025-01-09 19:38:47 +0200

    Update and rename visualassa.lua to visualasasa.lua

--- visualasasa.lua
+++ visualasasa.lua
@@ -504,7 +504,7 @@ do -- Player Metatable
                     end
                     --WeaponIcon.Image = 
                     WeaponIcon.Visible = WeaponIcon_Settings.Enabled
-                    WeaponIcon.Size = Vector2.new(Box_Size.X+5,60)
+                    WeaponIcon.Size = Vector2.new(40,40)
                     -- 
           
                     -- Health
