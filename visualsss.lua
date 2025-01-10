2f984c26405a45894ddcf92db7fc112175edf0c1
ashleywashere <ashleytrades412@gmail.com>
2025-01-10 21:39:55 +0200

    Update and rename visualss.lua to visualsss.lua

--- visualsss.lua
+++ visualsss.lua
@@ -555,7 +555,7 @@ do -- Player Metatable
                     --WeaponIcon.Image = 
                     WeaponIcon.Visible = WeaponIcon_Settings.Enabled
                     WeaponIcon.Size = Vector2.new(35,35)
-                    if false then
+                    if WeaponIcon.Data ~= tool_icon then
                     WeaponIcon.Data = tool_icon
                     end
                     -- 
