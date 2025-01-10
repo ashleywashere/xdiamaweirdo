3a010bacc32ab4e1a4adbabafac1e502a9fcdd63
ashleywashere <ashleytrades412@gmail.com>
2025-01-10 22:11:02 +0200

    Update and rename visualsssss.lua to visuals.lua

--- visuals.lua
+++ visuals.lua
@@ -557,6 +557,9 @@ do -- Player Metatable
                     oldtool_icon = tool_icon
                     WeaponIcon.Visible = WeaponIcon_Settings.Enabled
                     WeaponIcon.Size = Vector2.new(35,35)
+
+                    print(Images[tool_name])
+                    
                     if Images[tool_name] and Images[tool_name] ~= oldtool_icon then
                     WeaponIcon.Data = tool_icon
                     end
