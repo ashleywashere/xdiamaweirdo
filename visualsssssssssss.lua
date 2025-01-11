e2b7e8544636eb316dd7998b1b85a3d1dea3b0c1
ashleywashere <ashleytrades412@gmail.com>
2025-01-11 20:02:03 +0200

    Update and rename visualssssssssss.lua to visualsssssssssss.lua

--- visualsssssssssss.lua
+++ visualsssssssssss.lua
@@ -836,7 +836,7 @@ do -- ESP Functions
         local col = Data.Color or Data.color or Data.col or Data.Col or Color3.new(1, 1, 1)
         local out = Data.outline or Data.Outline or false
         local trans = Data.trans or Data.Trans or Data.Transparency or Data.transparency or Data.Alpha or Data.alpha or 1
-        local iconUrl = Images[Instance.Name] or Images["Hands"] -- Get icon or default to "Hands"
+        local iconUrl = CachedImages[Instance.Name] or CachedImages["Hands"] -- Get icon or default to "Hands"
         print(iconUrl)
         
         local Object = setmetatable({
