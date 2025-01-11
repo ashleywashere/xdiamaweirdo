57dc402a5995db6fc066f029061a34a470f1edbd
ashleywashere <ashleytrades412@gmail.com>
2025-01-11 20:31:49 +0200

    Update and rename visualssssss.lua to visualsssssss.lua

--- visualsssssss.lua
+++ visualsssssss.lua
@@ -855,8 +855,8 @@ do -- ESP Functions
             self:GetObject(Instance):Destroy()
         end
         local Components = Object.Components
-        Components.Name = VisualKit:Draw("Text", {Text = Object.Name, Color = col, Font = 2, Size = 13, Outline = out, Center = true, Transparency = trans})
-        Components.Addition = VisualKit:Draw("Text", {Text = Object.Addition.Text, Color = Object.Addition.Color, Font = 2, Size = 13, Outline = out, Center = true, Transparency = trans})
+        Components.Name = VisualKit:Draw("Text", {Text = Object.Name, Color = col, Font = Drawing.Fonts.System, Size = 13, Outline = out, Center = true, Transparency = trans})
+        Components.Addition = VisualKit:Draw("Text", {Text = Object.Addition.Text, Color = Object.Addition.Color, Font = Drawing.Fonts.System, Size = 13, Outline = out, Center = true, Transparency = trans})
         Components.Icon = VisualKit:Draw("Image", {Data = game:HttpGet(iconUrl), Size = Vector2.new(30, 30)}) -- Icon component
 
         self.Objects[Instance] = Object
