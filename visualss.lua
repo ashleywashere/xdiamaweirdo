cde7bca5a5d1fb9f2cc737abfd5a1373eb43986a
ashleywashere <ashleytrades412@gmail.com>
2025-01-11 20:47:36 +0200

    Update and rename visuals.lua to visualss.lua

--- visualss.lua
+++ visualss.lua
@@ -753,7 +753,7 @@ do  -- Object Metatable
         if On_Screen and Meter_Distance < ESP.Settings.Object_Maximal_Distance and Name then
             -- Name
             Name.Text = self.Name
-            Name.Position = VisualKit:V3_To_V2(Vector - Vector3.new(0, -13, 0))
+            Name.Position = VisualKit:V3_To_V2(Vector - Vector3.new(0, -17, 0))
             Name.Visible = true
 
             if self.Components.Icon then
